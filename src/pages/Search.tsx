fe226f4cd67b51f4db17ec6239b3136667955ff6
trevron <trevormakesmusic@gmail.com>
2022-04-17 03:14:15 +0200

    Cleaned up some unnecessary things from Search.tsx.

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,45 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { gql, request } from 'graphql-request';
-import SearchForm from '../components/SearchForm';
-import searchStore from '../store/SearchStore';
-import SearchResults from '../components/SearchResults';
+import React, { useEffect, useState } from "react";
+import { gql, request } from "graphql-request";
+import SearchForm from "../components/SearchForm";
+import searchStore from "../store/SearchStore";
+import SearchResults from "../components/SearchResults";
 
-// const allPlanets = gql`
-//     query {
-//       allPlanets {
-//         planets {
-//           name
-//           id
-//           climates
-//           diameter
-//           terrains
-//         }
-//       }
-//     }
-//   `;
-
-
-function Search() { 
-  // let loaded = false;
-  // useEffect(() =>{
-  //   if (!loaded) {
-  //     request('https://swapi-graphql.netlify.app/.netlify/functions/index', allPlanets).then(data => searchStore.setPlanets(data.allPlanets.planets));
-  //     loaded = true;
-  //   }
-  // }, []);
-
-
+function Search() {
   return (
     <div className="flex flex-col ">
-      <button onClick={searchStore.filterSearch}>Filter</button>
       <div className="text-gray-50 flex content-center align-center justify-center content-center">
-          <SearchForm />
+        <SearchForm />
       </div>
       <div>
-          <SearchResults />
+        <SearchResults />
       </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
