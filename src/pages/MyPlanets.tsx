ab66f6b73e809cd9bc53d4026a481ee0813b6350
trevron <trevormakesmusic@gmail.com>
2022-04-28 01:40:09 +0200

    Display a message if there are no saved planets.

--- src/pages/MyPlanets.tsx
+++ src/pages/MyPlanets.tsx
@@ -6,19 +6,29 @@ import { useRootStore } from "../store/RootStoreProvider";
 
 /**
  *  Observed list of saved planets
- * 
+ *
  */
 
 function MyPlanetsList() {
   const store = useRootStore();
   return (
     <div>
-      {store.planet.planets.map((planet) => (
-        // Regex replaces spaces with underscores
-        <Link key={planet.id} to={`/planet/${planet.name.replace(/ /g,"_")}`} state={{id: planet.id, from: "/myplanets"}}>
-          <PlanetCard planet={planet} />
-        </Link>
-      ))}
+      {store.planet.planets.length === 0 ? (
+        <h1 className="text-amber-400 font-bold text-xl flex justify-center self-center">
+          Looking a little empty... Try adding some planets from a search!
+        </h1>
+      ) : (
+        store.planet.planets.map((planet) => (
+          // Regex replaces spaces with underscores
+          <Link
+            key={planet.id}
+            to={`/planet/${planet.name.replace(/ /g, "_")}`}
+            state={{ id: planet.id, from: "/myplanets" }}
+          >
+            <PlanetCard planet={planet} />
+          </Link>
+        ))
+      )}
     </div>
   );
 }
@@ -28,7 +38,9 @@ const MyPlanetsObserver = observer(MyPlanetsList);
 function MyPlanets() {
   return (
     <>
-      <h1 className="flex justify-center text-4xl font-bold text-gray-50 uppercase">My Planets</h1>
+      <h1 className="flex justify-center text-4xl font-bold text-gray-50 uppercase">
+        My Planets
+      </h1>
       <MyPlanetsObserver />
     </>
   );
